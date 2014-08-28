dbea027d146cf674ccfe6cf66cd23d4d7eeb5077
Matt Bertino <mattbertino@gmail.com>
2014-08-28 16:39:43 -0400

    set up the sidebar and the js to make it responsive

--- Brocfile.js
+++ Brocfile.js
@@ -8,6 +8,8 @@ var app = new EmberApp();
 app.import('vendor/firebase-simple-login/firebase-simple-login.js');
 app.import('vendor/bootstrap/dist/js/bootstrap.js');
 app.import('vendor/bootstrap/dist/css/bootstrap.css');
+app.import('vendor/jasny-bootstrap/dist/css/jasny-bootstrap.min.css');
+app.import('vendor/jasny-bootstrap/dist/js/jasny-bootstrap.min.js');
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
--- app/router.js
+++ app/router.js
@@ -7,23 +7,23 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('application', { path: '/' }, function(){
 	  this.resource('login', function(){
-		  this.route('forgetpassword', {path: '/forgotpassword'});	  
+		  this.route('forgetpassword', {path: '/forgotpassword'});
 	  });
 	  this.resource('dashboard',{path: 'dashboard'}, function(){
 		  this.resource('profile');
 		  this.resource('newconversation', function(){
-			  this.route('content');		  	
+			  this.route('content');
 		  });
-		  this.resource('messages',{path: 'all-messages'})	
-		  this.resource('unread', {path: 'unread-messages'})	
-		  this.resource('events',{path: 'all-events'})	
-		  this.resource('message', {path: 'message/:message_id'}, function (){	  	
+		  this.resource('messages',{path: 'all-messages'})
+		  this.resource('unread', {path: 'unread-messages'})
+		  this.resource('events',{path: 'all-events'})
+		  this.resource('message', {path: 'message/:message_id'}, function (){
 			  this.route('reply');
 			  this.route('share');
 			  this.route('attach');
 		  });		  	
 		  this.resource('careteam', function(){
-			  this.route('add');		  	
+			  this.route('add');
 		  });
 	  });
   });
